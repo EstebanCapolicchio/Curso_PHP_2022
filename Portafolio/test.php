94ffdc4944b48ed52ceb44db22cc863240dc76c1
EstebanCapolicchio <estebancapolicchio@gmail.com>
2022-12-12 19:17:41 -0300

    * Portafolio: testeo movil 2

--- Portafolio/test.php
+++ Portafolio/test.php
@@ -1,40 +1,57 @@
 <?php
     Class Departamento{
-        public $a;
-        private $b;
+        public $direccion;
+        public $cantidad_pisos;
+        public $cantidad_departamentos;
+        private $encargados;
 
-        public function __construct($p1, $p2){
-            $this->a = $p1;
-            $this->b = $p2;
+        public function __construct($direccion, $cantidad_pisos, $cantidad_departamentos, $encargado){
+            $this->direccion = $direccion;
+            $this->cantidad_pisos = $cantidad_pisos;
+            $this->cantidad_departamentos = $cantidad_departamentos;
+            $this->encargados = [$encargado];
         }
 
-        public function method1($p1, $p2){
-            $aux = $a + $b;
-            return $aux;
+        public function despedir_encargado($encargado){
+            //verificar que el encargado esté en la lista
+            if($encargados[$encargado].obtener_faltas() <= 42){
+                //pagar indemnizacion
+            }
+            //eliminar al encargado de la lista
         }
 
-        private function method2($p1, $p2){
-            $aux = $a - $b;
-            return $aux;
+        public function contratar_encargado($encargado){
+            //verificar que el encargado no esté en la lista
+            //agregar al encargado a la lista
         }
     }
     Class Empleado{
-        public $a;
-        private $b;
+        private $nombre;
+        private $apellido;
+        private $dni;
+        private $faltas;
 
-        public function __construct($p1, $p2){
-            $this->a = $p1;
-            $this->b = $p2;
+        public function __construct($nombre, $apellido, $dni){
+            $this->nombre = $nombre;
+            $this->apellido = $apellido;
+            $this->dni = $dni;
+            $this->faltas = 0;
         }
 
-        public function method1($p1, $p2){
-            $aux = $a + $b;
-            return $aux;
+        public function imprimir(){
+            return this.nombre + " " + this.apellido+ " " + this.dni;
         }
 
-        private function method2($p1, $p2){
-            $aux = $a - $b;
-            return $aux;
+        public function obtener_faltas(){
+            return this.faltas;
+        }
+
+        public function agregar_falta(){
+            this.faltas += 1;
         }
     }
+
+    $t1 = new Empleado("Pepe", "Fulanito", "11111111");
+    echo($t1.imprimir);
+
 php?>
